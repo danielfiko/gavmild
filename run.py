6fe9b9fcf5ee879aca58928178e94c1d13515f54
Daniel <daniel.fiko@gmail.com>
2022-01-22 22:28:16 +0100

    Added SSL certificate

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from app import app, db
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, ssl_context=("/etc/letsencrypt/live/gavmild.sekta.no/fullchain.pem",
+                                     "/etc/letsencrypt/live/gavmild.sekta.no/privkey.pem"))
