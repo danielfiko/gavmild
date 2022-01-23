121cbf6589ed363c294249d2673808d732f8c964
danielfiko <daniel.fiko@gmail.com>
2022-01-23 14:46:02 +0000

    Removed SSL-certificate and debug parameters

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
 from app import app, db
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True, ssl_context=("/etc/letsencrypt/live/gavmild.sekta.no/fullchain.pem",
-                                     "/etc/letsencrypt/live/gavmild.sekta.no/privkey.pem"))
+    app.run()
