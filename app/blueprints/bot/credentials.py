367ff2ae05133096e5a36241df52a475a724b3d4
danielfiko <daniel.fiko@gmail.com>
2022-02-06 23:26:14 +0100

    Delete credentials.py

--- app/blueprints/bot/credentials.py
+++
@@ -1,8 +0,0 @@
-
-TOKEN = "5167073151:AAGTDmUTLYVIBD7mx_tbxIwxH27zENW3kdM"
-bot_user_name = "onske_bot"
-URL = "https://gavmild.sekta.no/bot"
-
-# To protect some public URLs
-CUSTOM_USERNAME = 'daniel'
-CUSTOM_PASSWORD = 'passord6'
